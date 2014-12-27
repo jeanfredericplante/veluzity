d9109ae70f828b7f20583e2a6fd67cd8d8d1effc
Jean Frederic Plante <jeanfredericplante@JFSSD.local>
2014-12-27 12:05:02 -0800

    Initial Commit

---
+++ Speed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Speed
+//
+//  Created by Jean Frederic Plante on 12/27/14.
+//  Copyright (c) 2014 Jean Frederic Plante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
