d67658b1fdcd333706cae00aa05e4d59b59028fd
Jean Frederic Plante <jeanfredericplante@JFSSD.local>
2014-12-28 10:07:12 -0800

    setting up weather and location components

---
+++ Speed/WeatherComponent.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherComponent.swift
+//  Speed
+//
+//  Created by Jean Frederic Plante on 12/28/14.
+//  Copyright (c) 2014 Jean Frederic Plante. All rights reserved.
+//
+
+import Foundation
---
+++ SpeedTests/LocationComponentTests.swift
@@ -0,0 +1,9 @@
+//
+//  LocationComponentTests.swift
+//  Speed
+//
+//  Created by Jean Frederic Plante on 12/28/14.
+//  Copyright (c) 2014 Jean Frederic Plante. All rights reserved.
+//
+
+import Foundation
