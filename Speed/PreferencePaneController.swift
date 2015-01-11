6bdc57820bb6c09827048875b16b08159f57e4c7
Jean-Frederic Plante <jfrederic.plante@gmail.com>
2015-01-10 20:50:08 -0800

    fixed trailing outlets in the storyboard

--- Speed/PreferencePaneController.swift
+++ Speed/PreferencePaneController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class PreferencePaneController: UIViewController {
 
+class PreferencePaneController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,14 +23,9 @@ class PreferencePaneController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
+    // MARK: Buttons
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+      
 
+    
 }
