e86a8fa0bb9d31a3c4f83d6a537e8f5efe0783b2
Jean-Frederic Plante <jfrederic.plante@gmail.com>
2015-04-01 23:02:51 -0700

    first display of the speed in apple watch

--- Veluzity WatchKit Extension/InterfaceController.swift
+++ Veluzity WatchKit Extension/InterfaceController.swift
@@ -11,17 +11,19 @@ import Foundation
 import VeluzityKit
 
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, LocationUpdateDelegate {
 
     @IBOutlet weak var speedLabel: WKInterfaceLabel!
     @IBOutlet weak var speedUnitLabel: WKInterfaceLabel!
     
     
     let userLocation = LocationModel()
+
     
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
+        userLocation.delegate = self
+ 
         // Configure interface objects here.
     }
 
@@ -37,7 +39,12 @@ class InterfaceController: WKInterfaceController {
     
     
     private func updateSpeed() {
-        
+        let speed = localizeSpeed(userLocation.speed, isMph: true)
+        speedLabel.setText(String(format: "%.0f", speed))
+    }
+    
+    func didUpdateLocation() {
+        updateSpeed()
     }
 
 }
