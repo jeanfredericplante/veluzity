d59f8a308eb921c1c909683b8f9097286a854a40
Jean-Frederic Plante <jfrederic.plante@gmail.com>
2015-06-21 10:44:12 -0700

    adds flurry analytics for watch

---
+++ Veluzity WatchKit Extension/Veluzity WatchKit Extension-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Veluzity WatchKit Extention-Bridging-Header.h
+//  Veluzity
+//
+//  Created by Jean Frederic Plante on 6/21/15.
+//  Copyright (c) 2015 Fantastic Whale Labs. All rights reserved.
+//
+
+#import "FlurryWatch.h"
